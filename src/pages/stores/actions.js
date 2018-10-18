98d885d2ba85efd75503822f15a52e9f92450fd4
JDEast1029 <852778359@qq.com>
2018-10-18 11:36:47 +0800

    update global action

--- src/pages/stores/actions.js
+++ src/pages/stores/actions.js
@@ -8,31 +8,38 @@ export const actions = {
 	request(store, opts = {}) {
 		const {
 			url: mutation,
+			redirectMutation, // 重定向Mutation
 			param,
-			noLoading = true,
+			pending,
+			fail,
+			noLoading,
 			...rest
 		} = opts;
+
+		let _mutation = redirectMutation || mutation;
+
 		if (!API_ROOT[mutation]) {
 			console.error('请求地址不存在');
 			return !1;
 		}
-
-		// noLoading 为 false，则必须要写_ON的mutation
-		!noLoading && store.commit(`${mutation}_ON`, { param });
+		
+		// pending 为 false，则必须要写_ON的mutation
+		pending && store.commit(`${_mutation}_PENDING`, { param });
 		return net.ajax({
 			url: API_ROOT[mutation],
 			param,
+			noLoading: param.page === undefined ? noLoading : true,
 			...rest
 		}).then((res) => {
 			const { data } = res;
-			store.commit(`${mutation}_SUCCESS`, {
+			store.commit(`${_mutation}_SUCCESS`, {
 				data,
 				param,
 				// ...rest
 			});
 			return res;
 		}).catch((error) => {
-			store.commit(`${mutation}_ERROR`, { param });
+			fail && store.commit(`${_mutation}_FAIL`, { param });
 			return Promise.reject(error);
 		});
 	}
