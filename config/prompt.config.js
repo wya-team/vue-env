ebc50e03ef2211756c31ed3267fea6052be64be9
Mr.Z <zrd0921@qq.com>
2019-02-26 22:33:11 +0800

    update: npm install

--- config/prompt.config.js
+++ config/prompt.config.js
@@ -3,7 +3,9 @@
  */
 const { resolve } = require('path');
 const { prompt, Separator } = require('inquirer');
+const { exec } = require('shelljs');
 const fs = require('fs-extra');
+
 const { routes } = require('./templates/routes');
 
 const directory = resolve(__dirname, '../src/pages/containers/');
@@ -22,10 +24,31 @@ fs.readdirSync(directory).forEach((file) => {
 });
 const question = [
 	{
+		type: 'confirm',
+		name: 'install',
+		message: 'npm install?',
+		default: false
+	},
+	{
 		type: 'input',
 		name: 'port',
 		message: 'port:',
-		default: '8082'
+		default: '8082',
+		choices(answers) {
+			if (answers.install) {
+				let done = this.async();
+				exec('npm install', { silent: true }, (code, stdout, stderr) => {
+					console.log('Exit code:', code);
+					console.log('Program output:', stdout);
+					console.log('Program stderr:', stderr);
+
+					if (code === 0) {
+						done(null, true);
+					}
+
+				});
+			}
+		}
 	},
 	{
 		type: 'list',
@@ -40,14 +63,13 @@ const question = [
 		message: 'Select modules:',
 		pageSize: targetModules.length + 1,
 		choices: targetModules,
-		validate: (answer) => {
+		validate(answer) {
 			if (answer.length < 1) {
 				return '至少选择一个模块, 使用Space键选中';
 			}
 			return true;
 		}
 	}
-	
 ];
 prompt(question).then((result = {}) => {
 	let { isSelectAll, modules, ...rest } = result;
