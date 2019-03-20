d614e1de53e2a0fc26e32efffd8e367f5d7f0419
JDEast1029 <852778359@qq.com>
2019-03-20 15:24:52 +0800

    update

--- src/pages/stores/modules/root.js
+++ src/pages/stores/modules/root.js
@@ -1,11 +1,9 @@
 import __tpl__ from './__tpl__/root';
 import login from './login/root';
-import setting from './setting/root';
 import layout from './layout/root';
 
 export default {
 	...__tpl__,
 	...layout,
 	...login,
-	...setting
 };
