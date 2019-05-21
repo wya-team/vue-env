7a4cf9f8a582e215a5dc26571e2bcc7c50e53d13
Mr.Z <zrd0921@qq.com>
2019-05-22 04:48:07 +0800

    update

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -3,6 +3,9 @@ module.exports = {
 		require('postcss-import')({}),
 		require('postcss-flexbugs-fixes')({}),
 		require('precss')({}),
+		require('cssnano')({
+			preset: 'default',
+		}),
 		require('autoprefixer')({
 			// 不删除老式写法
 			remove: false,
